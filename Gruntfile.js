9f9654373d5e5ba5ce3f86e8dd87f26a07f51e0b
serbanstef <serbans@gmail.com>
2018-03-04 13:27:40 +0200

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        compress: {
+            main: {
+                options: {
+                    archive: 'employeeservice.zip',
+                    pretty: true
+                },
+                expand: true,
+                cwd: './',
+                src: ['Employee.php', 'service.php','employees-rest-api.php', 'manifest.json', 'start.sh', 'timer.sh'],
+                dest: './'
+            }
+        }
+    });
+    // Default task(s).
+    grunt.registerTask('default', ['compress']);
+};
